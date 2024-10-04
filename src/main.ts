5004f1e3a0912f6ef7e686f05e9e951f38312c95
Harish0705 <harish.balachandar@nzme.co.nz>
2024-10-04 14:06:48 +1300

    add helmet and cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
-async function bootstrap() {
+async function main() {
   const app = await NestFactory.create(AppModule);
+  // setting CORS for specific origin
+  /* const corsOptions = {
+    origin: 'http://test-taskmanagement.com',
+    optionsSuccessStatus: 200,
+  }; */
+  app.use(helmet());
+  // app.enableCors(corsOptions);
   app.use(cookieParser(process.env.JWT_SECRET));
   await app.listen(5000);
 }
-bootstrap();
+main();
